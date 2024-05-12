1016ab636c19184fde46107d6d39b2b73dc2bb97
WeiHong <tanweihong0104@gmail.com>
2024-05-12 18:32:27 +0800

    nav and drawer added

--- app/src/main/java/com/example/mobile_assignment/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assignment/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.mobile_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mobile_assignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.host)!!.findNavController() }
+    private lateinit var abc: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        abc = AppBarConfiguration(
+            setOf(
+                R.id.home2,
+                R.id.workoutHome
+            ),
+            binding.root
+        )
+
+        setupActionBarWithNavController(nav, abc)
+        binding.bv.setupWithNavController(nav)
+        binding.nv.setupWithNavController(nav)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp(abc)
     }
 }
