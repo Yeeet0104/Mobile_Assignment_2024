cbe65b3cfb60b812e2408d24e3e8ba74b949f292
WeiHong <tanweihong0104@gmail.com>
2024-05-22 14:41:24 +0800

    Alot of feature Doned

--- app/src/main/java/util/OpenAIApi.kt
+++ app/src/main/java/util/OpenAIApi.kt
@@ -6,7 +6,6 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
-
 interface OpenAIApi {
     @Headers("Authorization: Bearer sk-proj-ZBhCYEhfzKNsZ1BNg1p1T3BlbkFJOm1ntOLDgDSovEJy0b5v")
     @POST("v1/chat/completions")
